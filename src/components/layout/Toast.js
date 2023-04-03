5f9e8a28f3126ef10301df2f7850c4ae14b1698c
Xdebx <86518562+Xdebx@users.noreply.github.com>
2023-04-03 13:59:35 +0800

    Initial commit

---
+++ src/components/layout/Toast.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const Toast = (message, type) => {
+    switch (type) {
+        case "success":
+            return toast.success(
+                <div>
+                    <p>{message}</p>
+                </div>, {
+                position: toast.POSITION.BOTTOM_CENTER
+            }
+            );
+        case "error":
+            return toast.error(
+                <div>
+                    <p>{message}</p>
+                </div>, {
+                position: toast.POSITION.BOTTOM_CENTER
+            }
+            );
+        case "warning":
+            return toast.warning(
+                <div>
+                    <p>{message}</p>
+                </div>, {
+                position: toast.POSITION.BOTTOM_CENTER
+            }
+            );
+        default:     
+    }
+};
+export default Toast;
