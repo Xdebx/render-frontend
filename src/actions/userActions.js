6f666c3c8a763326b016bfe7076a438462f89948
Xdebx <86518562+Xdebx@users.noreply.github.com>
2023-04-05 10:30:10 +0800

    Post Logout

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -141,7 +141,8 @@ export const logout = () => async (dispatch) => {
 
     try {
 
-        await axios.get(`${process.env.REACT_APP_API}/api/v1/logout`, {withCredentials:true})
+        // await axios.get(`${process.env.REACT_APP_API}/api/v1/logout`, {withCredentials:true})
+        await axios.post(`${process.env.REACT_APP_API}/api/v1/logout`, { withCredentials: true })
 
         dispatch({
 
